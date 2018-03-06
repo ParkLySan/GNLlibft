88916a4de0b6c976e1371dc907fac3c9bab2f0db
Lisa TOULZAC <litoulza@e2r13p11.42.fr>
2018-03-06 18:08:10 +0100

    all

---
+++ src/strings/ft_strndup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: litoulza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/06 18:07:42 by litoulza          #+#    #+#             */
+/*   Updated: 2018/03/06 18:07:45 by litoulza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ftstring.h"
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	char			*res;
+	unsigned int	i;
+
+	i = 0;
+	res = ft_malloc(sizeof(char) * (n + 1));
+	if (res == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
